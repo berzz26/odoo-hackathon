3afe3ea7bb5485cdf1a3e2c2a0d10d1819b51cb1
harshilpatel05 <patelharshil2005@gmail.com>
2025-07-12 18:22:09 +0530

    bell icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Route path="signup" element={<Signup />} />
       <Route path="login" element={<Login />} />
       <Route path='askquestion' element = {<AskQuestion/>}/>
-      <Route path='answers' element={<Answers/>}/>
+      <Route path='answers/:id' element={<Answers/>}/>
     </Routes>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   const navigate = useNavigate();
 
   return (
     <div className="w-screen">
-    <nav className="bg-gray-800 w-full px-4 py-4 mb-4">
-      <div className="flex justify-between items-center w-full px-4">
-        <h1 className="text-xl font-bold text-blue-400">StackIt</h1>
-        <div className="flex gap-2">
-          <button 
-            onClick={() => navigate("/login")}
-            className="border border-white px-4 py-1 rounded-full hover:bg-gray-700 transition"
-          >
-            Login
-          </button>
-          <button
-            onClick={() => navigate("/signup")}
-            className="border border-white px-4 py-1 rounded-full hover:bg-gray-700 transition"
-          >
-            SignUp
-          </button>
+      <nav className="bg-gray-800 w-full px-4 py-4 mb-4">
+        <div className="flex justify-between items-center w-full px-4">
+          <h1 className="text-xl font-bold text-blue-400">StackIt</h1>
+          <div className="flex gap-2 items-center">
+            <button 
+              onClick={() => navigate("/login")}
+              className="border border-white px-4 py-1 rounded-full hover:bg-gray-700 transition"
+            >
+              Login
+            </button>
+            <button
+              onClick={() => navigate("/signup")}
+              className="border border-white px-4 py-1 rounded-full hover:bg-gray-700 transition"
+            >
+              SignUp
+            </button>
+            <button
+              onClick={() => navigate("/notifications")}
+              className="border border-white px-4 py-1 rounded-full hover:bg-gray-700 transition"
+            >
+              <FontAwesomeIcon icon={faBell} className="text-white text-xl cursor-pointer" />
+            </button>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </div>
   );
 };
