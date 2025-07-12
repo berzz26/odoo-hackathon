93c838d9e16874bbeed4b98fdaebec80d59caddf
berzz26 <aumtamboli2233@gmail.com>
2025-07-12 11:49:07 +0530

    feat: add auth routes with RBAC

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,108 @@
+import prisma from "../src/config/db.js"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import z from 'zod'
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const signup = async (req, res) => {
+    const userSchema = z.object({
+        username: z.string(),
+        email: z.email(),
+        password: z.string().min(6),
+    })
+
+    const validate = userSchema.safeParse(req.body)
+
+    if (!validate.success) {
+        return res.status(400).json({
+            msg: "Validation failed",
+            errors: validate.error.errors
+        })
+    }
+
+    const { email, password, username } = req.body;
+
+    try {
+        // check if user already exists in db
+        const doesExist = await prisma.user.findUnique({ where: { email } })
+        if (doesExist) {
+            return res.status(400).json({
+                msg: "User already exists"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                email,
+                username,
+                password: hashedPassword,
+                role: 'USER'
+            }
+        })
+
+
+        res.status(201).json({ msg: 'User added successfully' })
+
+    } catch (error) {
+        console.error('Signup error:', error);
+        res.status(500).json({ msg: 'Server error', error: error.message })
+    }
+}
+
+export const login = async (req, res) => {
+    const userSchema = z.object({
+        email: z.email(),
+        password: z.string().min(1),
+    })
+
+    const validate = userSchema.safeParse(req.body)
+
+    if (!validate.success) {
+        return res.status(400).json({
+            msg: "Validation failed",
+            errors: validate.error.errors
+        })
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email }
+        })
+
+        if (!user) {
+            return res.status(400).json({
+                msg: "User does not exist"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                msg: "Invalid credentials"
+            })
+        }
+
+        const token = jwt.sign({
+            userId: user.id,
+            userEmail: user.email,
+            role: user.role
+        }, JWT_SECRET, {
+            expiresIn: '7d'
+        })
+
+        res.json({
+            msg: "Login successful",
+            authToken: token
+        })
+
+    } catch (error) {
+        console.error('Login error:', error);
+        res.status(500).json({ msg: 'Server error', error: error.message })
+    }
+}
