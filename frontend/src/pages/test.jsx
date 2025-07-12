1797104758a28f1c95ed6fb859068f2099259fb5
harshilpatel05 <patelharshil2005@gmail.com>
2025-07-12 10:39:24 +0530

    tailwind config

---
+++ frontend/src/pages/test.jsx
@@ -0,0 +1,12 @@
+import TextEditor from '../components/TextEditor'; // or TiptapEditor if that's the actual file
+
+const Test = () => {
+  return (
+    <div>
+      <h1>Text Editor Testing</h1>
+      <TextEditor />
+    </div>
+  );
+};
+
+export default Test;
